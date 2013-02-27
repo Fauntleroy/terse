0241926306f6fd2c0078f5b94efe23c3986d716b
Timothy Kempf <the.kaempfer@gmail.com>
2013-02-26 21:02:44 -0800

    set up basic client side assets

--- terse.js
+++ terse.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var express_handlebars = require('express3-handlebars');
+var igneous = require('igneous');
 
 var router = express();
 var handlebars = express_handlebars({
@@ -10,6 +11,53 @@ var handlebars = express_handlebars({
 
 router.engine( 'hbs', handlebars );
 router.set( 'view engine', 'hbs' );
+
+var igneous_middleware = igneous({
+	root: __dirname +'/assets',
+	minify: false,
+	flows: [
+		{
+			route: 'styles/terse.css',
+			type: 'css',
+			base: 'styles',
+			paths: [
+				'vendor/bootstrap',
+				'vendor/font-awesome',
+				'vendor/codemirror',
+				'terse.styl'
+			]
+		},
+		{
+			route: 'scripts/terse.js',
+			type: 'js',
+			base: 'scripts',
+			paths: [
+				'vendor/jquery',
+				'vendor/bootstrap',
+				'vendor/underscore',
+				'vendor/backbone',
+				'vendor/handlebars',
+				'terse.js',
+				'models',
+				'collections',
+				'views',
+				'routers',
+				'application.js'
+			]
+		},
+		{
+			route: 'templates/terse.js',
+			type: 'jst',
+			jst_lang: 'handlebars',
+			jst_namespace: 'templates',
+			base: 'templates',
+			paths: ['/']
+		}
+	]
+});
+
+router.use( igneous_middleware );
+router.use( express.static( __dirname + '/assets' ) );
 
 router.get( '/', function( req, res ){
 
