9cfdcab7fdbdfdec8e112afc6e6af10f300be2e3
Timothy Kempf <the.kaempfer@gmail.com>
2013-04-27 16:03:11 -0700

    Resolved issue with GitHub login Added User-Agent to GitHub Strategy fixes #16

--- terse.js
+++ terse.js
@@ -1,14 +1,18 @@
+// Process environment variables
+var VERSION = require('./package.json').version;
 var GITHUB_APP_ID = process.env.TERSE_GITHUB_APP_ID;
 var GITHUB_APP_SECRET = process.env.TERSE_GITHUB_APP_SECRET;
 var URL = process.env.TERSE_URL || 'http://terse.jit.su';
 var REDIS_PORT = parseInt( process.env.TERSE_REDIS_PORT, 10 ) || 6379;
 var REDIS_HOST = process.env.TERSE_REDIS_HOST || '127.0.0.1';
 var REDIS_AUTH = process.env.TERSE_REDIS_AUTH;
+var HTTP_PORT = process.env.TERSE_HTTP_PORT || 8080;
+var USER_AGENT = 'Terse/'+ VERSION;
 
+// Configure the web server
 var express = require('express');
 var expose = require('express-expose');
 var express_handlebars = require('express3-handlebars');
-
 var router = express();
 var handlebars = express_handlebars({
 	extname: '.hbs',
@@ -18,18 +22,20 @@ var handlebars = express_handlebars({
 
 router.engine( 'hbs', handlebars );
 router.set( 'view engine', 'hbs' );
-
 router.use( express.static( __dirname + '/assets' ) );
-
-//Passport stuff
-
-var passport = require('passport');
-
 router.use( express.cookieParser() );
 router.use( express.bodyParser() );
 router.use( express.session({
 	secret: 'terseness is tersy'
 }) );
+
+router.configure( 'development', function(){
+	router.use( express.logger() );
+});
+
+//Passport stuff
+var passport = require('passport');
+
 router.use( passport.initialize() );
 router.use( passport.session() );
 
@@ -48,7 +54,10 @@ passport.deserializeUser( function( obj, done ){
 passport.use( new GithubStrategy({
 	clientID: GITHUB_APP_ID,
 	clientSecret: GITHUB_APP_SECRET,
-	callbackURL: URL +'/auth/github/callback'
+	callbackURL: URL +'/auth/github/callback',
+	customHeaders: {
+		'User-Agent': USER_AGENT
+	}
 }, function( access_token, refresh_token, profile, done ){
 
 	var user = {
@@ -65,15 +74,7 @@ passport.use( new GithubStrategy({
 
 }));
 
-var github_auth_middleware = passport.authenticate( 'github', {
-	successRedirect: '/',
-	failureRedirect: '/login'
-});
-
-router.get( '/auth/github',	passport.authenticate( 'github', {
-	scope: 'gist'
-}));
-router.get( '/auth/github/callback', github_auth_middleware );
+// Routes
 router.get( '/', function( req, res ){
 
 	res.expose( req.user || {}, 'terse.user_data' );
@@ -82,6 +83,7 @@ router.get( '/', function( req, res ){
 	});
 
 });
+
 router.get( '/g/:id', function( req, res ){
 
 	res.expose( req.user || {}, 'terse.user_data' );
@@ -92,4 +94,21 @@ router.get( '/g/:id', function( req, res ){
 
 });
 
-router.listen( 8080 );
+router.get( '/auth/github',	passport.authenticate( 'github', {
+	scope: ['user', 'gist']
+}));
+
+router.get( '/auth/github/callback', passport.authenticate( 'github', {
+	successRedirect: '/',
+	failureRedirect: '/?err=login-failed'
+}));
+
+router.get( '/logout', function( req, res ){
+
+	req.logout();
+	res.redirect('/');
+
+});
+
+router.listen( HTTP_PORT );
+console.log( 'Terse listening on port '+ HTTP_PORT );
