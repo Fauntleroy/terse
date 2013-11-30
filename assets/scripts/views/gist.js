49c59abd6687c257ecb2650a5db0daab54d27342
Timothy Kempf <the.kaempfer@gmail.com>
2013-11-30 14:07:14 -0800

    add basic gist view

---
+++ assets/scripts/views/gist.js
@@ -0,0 +1,9 @@
+var _ = require('underscore');
+var Backbone = require('backbone');
+var $ = Backbone.$ = require('jquery');
+
+module.exports = Backbone.View.extend({
+	initialize: function(){
+		this.render();
+	}
+});
