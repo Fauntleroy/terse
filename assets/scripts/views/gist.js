ed0e7fa5b1f5e0292ac103f68e26632ee7d8d3eb
Timothy Kempf <the.kaempfer@gmail.com>
2013-11-30 20:00:58 -0800

    add result view

--- assets/scripts/views/gist.js
+++ assets/scripts/views/gist.js
@@ -6,6 +6,7 @@ var handlebars_helper = require('handlebars-helper');
 handlebars_helper.help( Handlebars );
 var templates = require('../../compiled/templates.js')( Handlebars );
 var EditorView = require('./editor.js');
+var ResultView = require('./result.js');
 
 module.exports = Backbone.View.extend({
 	template: templates.gist,
@@ -17,8 +18,13 @@ module.exports = Backbone.View.extend({
 		var html = this.template( this.model.toJSON() );
 		this.$el.html( html );
 		this.$editor = this.$('#editor');
+		this.$result = this.$('#result');
 		this.editor = new EditorView({
 			el: this.$editor,
+			model: this.model
+		});
+		this.result = new ResultView({
+			el: this.$result,
 			model: this.model
 		});
 	}
--- assets/scripts/views/result.js
+++ assets/scripts/views/result.js
@@ -1,48 +1,19 @@
-terse.Views.Result = Backbone.View.extend({
+var _ = require('underscore');
+var Backbone = require('backbone');
+var $ = Backbone.$ = require('jquery');
+var Handlebars = require('handlebars');
+var handlebars_helper = require('handlebars-helper');
+handlebars_helper.help( Handlebars );
+var templates = require('../../compiled/templates.js')( Handlebars );
 
-	template: terse.templates.result,
-
+module.exports = Backbone.View.extend({
+	template: templates.result,
 	initialize: function(){
-
-		_( this ).bindAll( 'render', 'update' );
-
-		this.listenTo( this.model, 'update sync', this.update );
-
+		this.listenTo( this.model, 'change:files', this.render );
+		this.render();
 	},
-
 	render: function(){
-
 		var html = this.template( this.model.toJSON() );
-		var $html = $.parseHTML( html );
-		this.$el.replaceWith( $html );
-		this.setElement( $html );
-
-		this.$iframe = this.$el.find('iframe');
-
-		return this;
-
-	},
-
-	// Update the contents of the result iframe
-	update: function(){
-
-		var html = '', css = '', js = '';
-		var extension_regex = /\.[a-z0-9]+$/i;
-		var files = this.model.get('files');
-		_( files ).each( function( value, key ){
-			var extension = extension_regex.exec( key ) || [];
-			extension = extension[0];
-			if( extension === '.html' ) html = value.content;
-			else if( extension === '.css' ) css = value.content;
-			else if( extension === '.js' ) js = value.content;
-		});
-		var iframe_content = '<head><style>'+ css +'</style></head><body>'+ html +'<script>'+ js +'</script></body>';
-		var iframe_doc = this.$iframe.contents()[0];
-
-		iframe_doc.open();
-		iframe_doc.write( iframe_content );
-		iframe_doc.close(); 
-
+		this.$el.html( html );
 	}
-
 });
