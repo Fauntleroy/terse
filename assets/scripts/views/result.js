7f0ed0001892c74f60c719aae029e29f9a3b4019
Timothy Kempf <the.kaempfer@gmail.com>
2013-12-01 12:35:24 -0800

    barebones preview pane

--- assets/scripts/views/result.js
+++ assets/scripts/views/result.js
@@ -6,14 +6,35 @@ var handlebars_helper = require('handlebars-helper');
 handlebars_helper.help( Handlebars );
 var templates = require('../../compiled/templates.js')( Handlebars );
 
+var VIEWABLE_TYPES = ['text/html'];
+
 module.exports = Backbone.View.extend({
 	template: templates.result,
 	initialize: function(){
+		_.bindAll( this, 'addFile', 'activeFile' );
 		this.listenTo( this.model, 'change:files', this.render );
+		this.files = [];
 		this.render();
 	},
 	render: function(){
-		var html = this.template( this.model.toJSON() );
+		var json = this.model.toJSON();
+		json.files = _.filter( json.files, function( file ){
+			return _.contains( VIEWABLE_TYPES, file.type );
+		});
+		var html = this.template( json );
 		this.$el.html( html );
+		this.$frame = this.$('iframe');
+		_.each( json.files, this.addFile );
+	},
+	addFile: function( file ){
+		this.files.push( file );
+		this.trigger( 'active', file );
+		this.activeFile( file );
+	},
+	activeFile: function( file ){
+		var iframe_doc = this.$frame.contents()[0];
+		iframe_doc.open();
+		iframe_doc.write( file.content );
+		iframe_doc.close();
 	}
 });
